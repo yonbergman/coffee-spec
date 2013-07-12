4dbefc772eb5a70891127055c21c0f5900c234c7
yon <Lighthawky@gmail.com>
2013-07-13 01:10:36 +0300

    fixing heroku and deprecation warnings - improved print

---
+++ config/initializers/enum_hotfix.rb
@@ -0,0 +1,1 @@
+require_relative '../../lib/enum_hotfix'
---
+++ lib/enum_hotfix.rb
@@ -0,0 +1,63 @@
+module Enumify
+  module Model
+    def enum(parameter, opts=[])
+
+      validates_inclusion_of parameter, :in => opts
+
+      const_set("#{parameter.to_s.pluralize.upcase}", opts)
+
+      define_method "#{parameter.to_s}" do
+        attr = read_attribute(parameter)
+        (attr.nil? || attr.empty?) ? nil : attr.to_sym
+      end
+
+      define_method "#{parameter.to_s}=" do |value|
+        send("_set_#{parameter.to_s}", value, false)
+      end
+
+      self.class_eval do
+
+        private
+
+        define_method "_set_#{parameter.to_s}" do |value, should_save|
+
+          value = value.to_sym
+          old = read_attribute(parameter) ? read_attribute(parameter).to_sym : nil
+          write_attribute(parameter, value.to_s)
+          save if should_save
+          send("#{parameter.to_s}_changed", old, value) if respond_to?("#{parameter.to_s}_changed", true) and old != value and !old.nil?
+          return value
+        end
+
+      end
+
+      opts.each do |opt|
+        raise "Collision in enum values method #{opt}" if respond_to?("#{opt.to_s}?") or respond_to?("#{opt.to_s}!") or respond_to?("#{opt.to_s}")
+
+        define_method "#{opt.to_s}?" do
+            send("#{parameter.to_s}") == opt
+        end
+
+        define_method "#{opt.to_s}!" do
+            send("_set_#{parameter.to_s}", opt, true)
+        end
+
+        scope opt.to_sym, -> {where(parameter.to_sym => opt.to_s)}
+      end
+
+      # We want to first define all the "positive" scopes and only then define
+      # the "negation scopes", to make sure they don't override previous scopes
+      opts.each do |opt|
+        # We need to prefix the field with the table name since if this scope will
+        # be used in a joined query with other models that have the same enum field then
+        # it will fail on ambiguous column name.
+        unless respond_to?("not_#{opt}")
+          scope "not_#{opt}", -> {where("#{self.table_name}.#{parameter} != ?", opt.to_s)}
+        end
+      end
+
+    end
+
+  end
+
+end
