a2bff52ee9ecf1421ccdf04d6fad82c982d0c3eb
yon <Lighthawky@gmail.com>
2016-01-21 16:39:50 +0200

    support both

--- app/models/drink_texter.rb
+++ app/models/drink_texter.rb
@@ -95,7 +95,7 @@ class DrinkTexter
       t += case drink.sugar_type
         when 'regular' then 'Sugar'
         when 'brown' then 'Brown Sugar'
-        when 'artificial_sweetner' then 'Artificial Sweetner'
+        when 'artificial_sweetener', 'artificial_sweetner' then 'Artificial Sweetner'
       end
     end
     t
